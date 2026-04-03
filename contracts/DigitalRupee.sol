d118c178b65fafb34ca60c9704a6d3683f0e34eb
Harsh Raj <harsh200519@example.com>
2026-04-03 20:07:18 +0530

    token minted

--- contracts/DigitalRupee.sol
+++ contracts/DigitalRupee.sol
@@ -7,13 +7,29 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 // digitalRupee contract
 contract DigitalRupee is ERC20, ERC20Burnable, AccessControl {
+//Creating  unique role identifiers
+//Using keccak256 (hash function) to generate secure role IDs
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    //Storing logs on blockchain
 
-    event TokensMinted(address indexed to , unit256 amount ,unit256 timestamp);
-    event TokensRevoked(address indexed from , unit256 amount, unit256 timestamp);
-    constructor() ERC20("DigitalRupee", "INRD"){
+    event TokensMinted(address indexed to, uintt256 amount, uintt256 timestamp);
+    event TokensRevoked(
+        address indexed from,
+        unit256 amount,
+        unit256 timestamp
+    );
+            //initiallizing ERC20
+    constructor() ERC20("DigitalRupee", "INRD") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
-    
-
+}
+//Only addresses with MINTER_ROLE can call this
+  function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+        emit TokensMinted(to, amount, block.timestamp);
+    }
+    function revokeTokens(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+        emit TokensRevoked(from, amount, block.timestamp);
+    }
